17b5582d77e7ce22cac760ef09d5bf789ae66265
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-08-01 19:59:16 -0300

    feat: implemetadas algumas ideias de estilização

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,15 +1,23 @@
+"use client"
+
 import Link from "next/link"
+import "../styles/navbar.css"
+import { usePathname } from 'next/navigation'
 
 export default function Navbar() {
+    
+    const pathname = usePathname()
+
     return (
-        <nav>
-            <ul>
-                <li><Link href={"/"}>home</Link></li>
-                <li><Link href={"about"}>sobre mim</Link></li>
-                <li><Link href={"projects"}>projetos</Link></li>
-                <li><Link href={"social_media"}>redes sociais</Link></li>
-                <li><Link href={"contact"}>entre em contato</Link></li>
-            </ul >
+        <nav className="navbar_container">
+            <ul className="navbar">
+                <li><Link href={"/"} className={pathname == "/" ? "active_section" : ""}>home</Link></li>
+                <li><Link href={"about"} className={pathname == "/about" ? "active_section" : ""}>sobre mim</Link></li>
+                <li><Link href={"skills"} className={pathname == "/skills" ? "active_section" : ""}>skills</Link></li>
+                <li><Link href={"projects"} className={pathname == "/projects" ? "active_section" : ""}>projetos</Link></li>
+                <li><Link href={"social_media"} className={pathname == "/social_media" ? "active_section" : ""}>redes sociais</Link></li>
+                <li><Link href={"contact"} className={pathname == "/contact" ? "active_section" : ""}>entre em contato</Link></li>
+            </ul>
         </nav>
     )
 }
