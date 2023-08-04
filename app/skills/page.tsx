71086f51b96a21fcc8efbc7684dca8acd0b4ad8a
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-08-04 12:13:41 -0300

    feat: implementação do código que lê o JSON e faz a renderização

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,5 +1,23 @@
-export default function Skills() {
+import skillsDataFromFile from "../../public/json/skills.json"
+
+async function getData() {
+  try {
+    const data = skillsDataFromFile
+    return data;
+  } catch (e: any) {
+    console.error(e.message);
+  }
+}
+
+export default async function Skills() {
+  const { skills }: any = await getData();
+  console.log(skills);
   return (
-    <h1>Skills</h1>
+
+    <>
+      {skills.map((skill: any) => (
+        <h1 key={skill.id}>{skill.title}</h1>
+      ))}
+    </>
   )
 }
