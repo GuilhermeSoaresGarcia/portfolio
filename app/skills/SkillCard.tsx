161343837ef2ae21eed8c0451db2d7770040142c
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-08-07 19:07:23 -0300

    feat: criada div com classe "card-titles" e implementada classe "card-img" na tag de imagem para facilitar a implementação de estilos

--- app/skills/SkillCard.tsx
+++ app/skills/SkillCard.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image"
 import { ISkill } from "./interfaces/ISkill"
 
-export default async function SkillCard({ title, subtitle, img, description }: ISkill) {
+export default function SkillCard({ title, subtitle, img, description }: ISkill) {
 
     return (
-        <div>
-            <h1>{title}</h1>
-            <h2>{subtitle}</h2>
-            <Image src={img} height="32" width="32" alt={title} />
+        <>
+            <div className="card-titles">
+                <h1>{title}</h1>
+                <h2>{subtitle}</h2>
+            </div>
+            <Image className="card-img" src={img} height="32" width="32" alt={title} />
+            <hr />
             <p>{description}</p>
-        </div>
+        </>
     )
 }
