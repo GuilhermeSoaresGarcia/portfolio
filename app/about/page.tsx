2cece8109ab26b9ebf22cbcaf107cc394fe93b6f
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-08-08 15:00:01 -0300

    feat: começando a escrever o texto da seção "sobre mim" e implementação da div com classe "about-container"

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,54 +1,16 @@
+/* eslint-disable react/no-unescaped-entities */
 export default function About() {
   return (
-    <>
-      <h1>Sobre mim</h1>
+    <div className="about-container">
+      <h1>Um pouco sobre mim</h1>
+      <h2>Um subtítulo qualquer</h2>
+      <hr />
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum atque mollitia officiis ratione veritatis necessitatibus quas labore velit, doloremque sed quibusdam laboriosam blanditiis magni repudiandae molestiae, et distinctio laudantium vitae!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae iste explicabo, eum provident quos laboriosam ad error impedit nihil natus eos? Accusamus eveniet assumenda ad cupiditate nobis eos non deserunt?
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam necessitatibus dolores labore? Magnam cupiditate aut velit rerum illo, officia voluptate harum ullam iure perferendis, id inventore sint ipsam adipisci obcaecati.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dignissimos harum, ducimus sit molestiae quas ratione, facere animi beatae laboriosam odit officiis, magni voluptatibus quia magnam expedita fugiat temporibus in!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. A maiores ratione laborum molestiae, alias consequuntur! Nihil officia vero facilis temporibus, libero eligendi repellendus, amet mollitia odit eius dicta deleniti. At!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum atque mollitia officiis ratione veritatis necessitatibus quas labore velit, doloremque sed quibusdam laboriosam blanditiis magni repudiandae molestiae, et distinctio laudantium vitae!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae iste explicabo, eum provident quos laboriosam ad error impedit nihil natus eos? Accusamus eveniet assumenda ad cupiditate nobis eos non deserunt?
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam necessitatibus dolores labore? Magnam cupiditate aut velit rerum illo, officia voluptate harum ullam iure perferendis, id inventore sint ipsam adipisci obcaecati.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dignissimos harum, ducimus sit molestiae quas ratione, facere animi beatae laboriosam odit officiis, magni voluptatibus quia magnam expedita fugiat temporibus in!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. A maiores ratione laborum molestiae, alias consequuntur! Nihil officia vero facilis temporibus, libero eligendi repellendus, amet mollitia odit eius dicta deleniti. At!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum atque mollitia officiis ratione veritatis necessitatibus quas labore velit, doloremque sed quibusdam laboriosam blanditiis magni repudiandae molestiae, et distinctio laudantium vitae!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae iste explicabo, eum provident quos laboriosam ad error impedit nihil natus eos? Accusamus eveniet assumenda ad cupiditate nobis eos non deserunt?
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam necessitatibus dolores labore? Magnam cupiditate aut velit rerum illo, officia voluptate harum ullam iure perferendis, id inventore sint ipsam adipisci obcaecati.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dignissimos harum, ducimus sit molestiae quas ratione, facere animi beatae laboriosam odit officiis, magni voluptatibus quia magnam expedita fugiat temporibus in!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. A maiores ratione laborum molestiae, alias consequuntur! Nihil officia vero facilis temporibus, libero eligendi repellendus, amet mollitia odit eius dicta deleniti. At!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum atque mollitia officiis ratione veritatis necessitatibus quas labore velit, doloremque sed quibusdam laboriosam blanditiis magni repudiandae molestiae, et distinctio laudantium vitae!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae iste explicabo, eum provident quos laboriosam ad error impedit nihil natus eos? Accusamus eveniet assumenda ad cupiditate nobis eos non deserunt?
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam necessitatibus dolores labore? Magnam cupiditate aut velit rerum illo, officia voluptate harum ullam iure perferendis, id inventore sint ipsam adipisci obcaecati.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dignissimos harum, ducimus sit molestiae quas ratione, facere animi beatae laboriosam odit officiis, magni voluptatibus quia magnam expedita fugiat temporibus in!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. A maiores ratione laborum molestiae, alias consequuntur! Nihil officia vero facilis temporibus, libero eligendi repellendus, amet mollitia odit eius dicta deleniti. At!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum atque mollitia officiis ratione veritatis necessitatibus quas labore velit, doloremque sed quibusdam laboriosam blanditiis magni repudiandae molestiae, et distinctio laudantium vitae!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae iste explicabo, eum provident quos laboriosam ad error impedit nihil natus eos? Accusamus eveniet assumenda ad cupiditate nobis eos non deserunt?
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam necessitatibus dolores labore? Magnam cupiditate aut velit rerum illo, officia voluptate harum ullam iure perferendis, id inventore sint ipsam adipisci obcaecati.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dignissimos harum, ducimus sit molestiae quas ratione, facere animi beatae laboriosam odit officiis, magni voluptatibus quia magnam expedita fugiat temporibus in!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. A maiores ratione laborum molestiae, alias consequuntur! Nihil officia vero facilis temporibus, libero eligendi repellendus, amet mollitia odit eius dicta deleniti. At!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum atque mollitia officiis ratione veritatis necessitatibus quas labore velit, doloremque sed quibusdam laboriosam blanditiis magni repudiandae molestiae, et distinctio laudantium vitae!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae iste explicabo, eum provident quos laboriosam ad error impedit nihil natus eos? Accusamus eveniet assumenda ad cupiditate nobis eos non deserunt?
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam necessitatibus dolores labore? Magnam cupiditate aut velit rerum illo, officia voluptate harum ullam iure perferendis, id inventore sint ipsam adipisci obcaecati.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dignissimos harum, ducimus sit molestiae quas ratione, facere animi beatae laboriosam odit officiis, magni voluptatibus quia magnam expedita fugiat temporibus in!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. A maiores ratione laborum molestiae, alias consequuntur! Nihil officia vero facilis temporibus, libero eligendi repellendus, amet mollitia odit eius dicta deleniti. At!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum atque mollitia officiis ratione veritatis necessitatibus quas labore velit, doloremque sed quibusdam laboriosam blanditiis magni repudiandae molestiae, et distinctio laudantium vitae!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae iste explicabo, eum provident quos laboriosam ad error impedit nihil natus eos? Accusamus eveniet assumenda ad cupiditate nobis eos non deserunt?
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam necessitatibus dolores labore? Magnam cupiditate aut velit rerum illo, officia voluptate harum ullam iure perferendis, id inventore sint ipsam adipisci obcaecati.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dignissimos harum, ducimus sit molestiae quas ratione, facere animi beatae laboriosam odit officiis, magni voluptatibus quia magnam expedita fugiat temporibus in!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. A maiores ratione laborum molestiae, alias consequuntur! Nihil officia vero facilis temporibus, libero eligendi repellendus, amet mollitia odit eius dicta deleniti. At!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum atque mollitia officiis ratione veritatis necessitatibus quas labore velit, doloremque sed quibusdam laboriosam blanditiis magni repudiandae molestiae, et distinctio laudantium vitae!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae iste explicabo, eum provident quos laboriosam ad error impedit nihil natus eos? Accusamus eveniet assumenda ad cupiditate nobis eos non deserunt?
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam necessitatibus dolores labore? Magnam cupiditate aut velit rerum illo, officia voluptate harum ullam iure perferendis, id inventore sint ipsam adipisci obcaecati.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dignissimos harum, ducimus sit molestiae quas ratione, facere animi beatae laboriosam odit officiis, magni voluptatibus quia magnam expedita fugiat temporibus in!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. A maiores ratione laborum molestiae, alias consequuntur! Nihil officia vero facilis temporibus, libero eligendi repellendus, amet mollitia odit eius dicta deleniti. At!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum atque mollitia officiis ratione veritatis necessitatibus quas labore velit, doloremque sed quibusdam laboriosam blanditiis magni repudiandae molestiae, et distinctio laudantium vitae!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae iste explicabo, eum provident quos laboriosam ad error impedit nihil natus eos? Accusamus eveniet assumenda ad cupiditate nobis eos non deserunt?
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam necessitatibus dolores labore? Magnam cupiditate aut velit rerum illo, officia voluptate harum ullam iure perferendis, id inventore sint ipsam adipisci obcaecati.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dignissimos harum, ducimus sit molestiae quas ratione, facere animi beatae laboriosam odit officiis, magni voluptatibus quia magnam expedita fugiat temporibus in!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. A maiores ratione laborum molestiae, alias consequuntur! Nihil officia vero facilis temporibus, libero eligendi repellendus, amet mollitia odit eius dicta deleniti. At!
-      </p>
-    </>
+        Desde pequeno sempre gostei bastante de entender o funcionamento das coisas, então eu costumava abrir os brinquedos e tirar os "motorezinhos" daqui,
+        colocar uma "lampadinha" ali, etc. Eu também costumava (e ainda o faço na verdade) consertar as coisas em casa: de radinhos de pilha, passando por
+        fechaduras de porta, até equipementos como fornos de microondas, máquinas de lavar e,
+        claro, computadores.
+      </p>      
+    </div>
   )
 }
