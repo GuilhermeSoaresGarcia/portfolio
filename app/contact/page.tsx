dc396a6ae5fdbfc6ae1d5ac152dc89f48c4d5b27
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-08-09 15:36:50 -0300

    feat: implementada a validação dos campos do formulário da página de contato usando a biblioteca zod

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,30 +1,81 @@
 "use client"
 
-import { useRef } from "react";
-import emailjs from '@emailjs/browser';
+import Link from "next/link";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { z } from "zod";
 
 export default function Contact() {
+  const [showUserFeedback, setShowUserFeedback] = useState<any>([]);
   const form: React.RefObject<HTMLFormElement> = useRef<HTMLFormElement>(null);
   const serviceId: String | undefined = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
   const templateId: String | undefined = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
   const publicKey: String | undefined = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
 
+  const FormSchema = z.object({
+    user_name:
+      z.string()
+        .nonempty({ message: 'O campo "Nome" é obrigatório' }),
+    user_email:
+      z.string()
+        .nonempty({ message: 'O campo "E-mail" é obrigatório' })
+        .email({ message: 'E-mail inválido' }),
+    message:
+      z.string()
+        .nonempty({ message: 'O campo "Mensagem" deve ser preenchido' })
+        .max(250, { message: "A mensagem não deve ultrapassar 250 caracteres" }),
+  });
+
   const sendEmail = (e: any) => {
     e.preventDefault();
 
-    emailjs.sendForm(serviceId as string, templateId as string, form.current!, publicKey as string)
-      .then((result) => {
-          console.log(result.text);
-          e.target.reset();
-          window.alert("Mensagem enviada com sucesso!");
-      }, (error) => {
-          console.log(error.text);
-      });
+    const { user_email, user_name, message } = e.target;
+
+    const formFields = {
+      user_email: user_email.value,
+      user_name: user_name.value,
+      message: message.value,
+    }
+
+    const validationResult = FormSchema.safeParse(formFields)
+
+    if (validationResult.success === false) {
+
+      const registeredErrors: Array<String> = [];
+
+      validationResult.error.issues.map((issue: any) => {
+        registeredErrors.push(issue.message);
+        setShowUserFeedback([...registeredErrors]);
+        setTimeout(() => setShowUserFeedback([""]), 5000);
+      })
+
+    } else {
+      setShowUserFeedback(["Mensagem enviada com sucesso!"]);
+      setTimeout(() => setShowUserFeedback([""]), 3000);
+    }
+
   }
 
+
+  // emailjs.sendForm(serviceId as string, templateId as string, form.current!, publicKey as string)
+  //   .then((result) => {
+  //     console.log(result.text);
+  //     e.target.reset();
+  //     window.alert("Mensagem enviada com sucesso!");
+  //   }, (error) => {
+  //     console.log(error.text);
+  //   });
+
   return (
-    <>
+    <div className="contact-container">
       <h1>Contato</h1>
+
+      <p>Para entrar em contato comigo, tu podes preencher o formulário abaixo ou me acionar pelo meu<Link href="https://www.linkedin.com/in/guilhermesoaresgarcia/" target="blank">Linkedin</Link></p>
+
+      {showUserFeedback.map((item: any) => {
+        return <div key={item.index}>{item}{item.index}</div>
+      })}
+
       <form ref={form} onSubmit={sendEmail}>
         <label>Nome:
           <input
@@ -47,10 +98,10 @@ export default function Contact() {
           </textarea>
         </label>
         <input
-          type="submit"          
+          type="submit"
           value="Enviar"
         />
       </form>
-    </>
+    </div>
   )
 }
