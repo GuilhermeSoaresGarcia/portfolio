602feaae97ac594fada8655fe35aea9ffe24363d
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-08-14 17:23:27 -0300

    refactor: componentização dos cards do projeto para melhorar a leitura e manutenção do código

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,5 @@
+import ProjectCard from "./RepoCard";
+
 // FONTE: https://nextjs.org/docs/app/building-your-application/data-fetching/fetching-caching-and-revalidating#fetching-data-on-the-server-with-fetch
 async function getData() {
   const url = process.env.GH_API_URL as string;
@@ -11,16 +13,19 @@ async function getData() {
 
 export default async function Projects() {
   const data = await getData();
-  const ghRepositories = data.filter((repo: any) => (repo.description !== null));
+  const ghRepositories = data.filter((repo: any) => (repo.description !== null));  
 
   return (
     <>
       {ghRepositories.map((item: any) => (
-        <div key={item.id}>
-          <h1>{item.name}</h1>
-          <h2>{item.description}</h2>
-          <h3>{item.html_url}</h3>
-          <h4>{item.language}</h4>
+        <div className="repo-card" key={item.id}>
+          <ProjectCard
+            name={item.name}
+            description={item.description}
+            html_url={item.html_url}
+            language={item.language}
+            topics={item.topics}
+          />
         </div>
       ))}
     </>
