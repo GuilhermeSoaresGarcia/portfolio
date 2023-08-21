59e255c64c1ab6472bc73377a3b0948bdabaad6e
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-08-21 11:23:25 -0300

    feat: implementação de um visual mais agradável à página de "not-found"

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,13 @@
+import Image from "next/image"
+import NotFoundImg from "../public/img/not-found.svg"
+import "../styles/not-found.css"
+
 export default function NotFound() {
-    return (        
-        <h1>404 - Page not found</h1>
+    return (
+        <div className="not-found-container">
+            <h1>404 - Opa... Algo de errado não está certo!</h1>
+            <h2>Página não encontrada</h2>
+            <Image src={NotFoundImg} alt="Not Found" />
+        </div>
     )
 }
