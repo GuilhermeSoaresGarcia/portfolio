22a870eac9fc493df38a3b2f20e7801f75bb22ad
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-08-21 10:44:56 -0300

    feat: implementação de uma nova div com classe 'skills-card-container' (que envolve somente os cards das skills) e implementação de título, subtítulo e texto descritivo à seção 'skills'

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -20,18 +20,30 @@ export default async function Skills() {
 
   return (
     <div className="skill-container">
-      {
-        skills.map((skill: ISkill) => (
-          <div className="skill-card" key={skill.id}>
-            <SkillCard
-              title={skill.title}
-              subtitle={skill.subtitle}
-              img={skill.img}
-              description={skill.description}
-            />
-          </div>
-        ))
-      }
+      <h1>Skills</h1>
+      <h2>Algumas das minhas &quot;hard-skills&quot;</h2>
+      <hr />
+      <p>
+        Abaixo estão listadas algumas das linguagens, ferramentas e tecnologias que 
+        considero mais relevantes e com as quais já trabalhei até o momento. A listagem 
+        não está em nenhuma ordem específica e coloquei um texto descritivo que não possui 
+        caráter técnico mas meramente alguma observação pessoal de porque aquilo tem alguma
+        importância para mim.
+      </p>
+      <div className="skills-card-container">
+        {
+          skills.map((skill: ISkill) => (
+            <div className="skill-card" key={skill.id}>
+              <SkillCard
+                title={skill.title}
+                subtitle={skill.subtitle}
+                img={skill.img}
+                description={skill.description}
+              />
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
