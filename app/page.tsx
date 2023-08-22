c0f20fd5a08dfa67d7a11398c48f599705933efc
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-08-22 14:53:50 -0300

    feat: implementação da página home

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
+import Image from "next/image";
+import smdlkasmd from "../public/img/gsg.jpg";
+import "../styles/home.css";
+
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <div className="home-container">
+      <h1>Guidje&apos;s Portfolio</h1>
+      <h2>Seja muito bem-vindo!</h2>
+      <hr />
+      <div className="home-content-container">
+        <p>
+        Aqui você encontra uma amostra dos meus projetos e trabalhos. Fique à vontade 
+        para explorar e espero que goste do que viu. Se tiver alguma dúvida ou quiser 
+        saber mais, é só me dar um toque!
+        </p>
+        <Image src={smdlkasmd} alt="Foto" />
+      </div>
+    </div>
   )
 }
