b77d90aceaa9d3c1d628c34afea04e8acc70d053
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-08-22 17:35:16 -0300

    refactor: remoção de fragmento desnecessário

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -12,30 +12,28 @@ export default function Navbar() {
     const pathname = usePathname()
 
     return (
-        <>
-            <nav className="navbar_container">
-                <ul className="navbar">
-                    <li><Link href={"/"} className={pathname == "/" ? "active_section" : ""}>home</Link></li>
-                    <li><Link href={"about"} className={pathname == "/about" ? "active_section" : ""}>sobre mim</Link></li>
-                    <li><Link href={"skills"} className={pathname == "/skills" ? "active_section" : ""}>skills</Link></li>
-                    <li><Link href={"projects"} className={pathname == "/projects" ? "active_section" : ""}>projetos</Link></li>
-                    <li><Link href={"contact"} className={pathname == "/contact" ? "active_section" : ""}>entre em contato</Link></li>
-                    <div className="social_media">
-                        <Link href={"https://github.com/GuilhermeSoaresGarcia"} target="_blank">
-                            <Image
-                                src={GitHubSVG}
-                                alt="GitHub"
-                            />
-                        </Link>
-                        <Link href={"https://www.linkedin.com/in/guilhermesoaresgarcia/"} target="_blank">
-                            <Image
-                                src={LinkedinSVG}
-                                alt="Linkedin"
-                            />
-                        </Link>
-                    </div>
-                </ul>
-            </nav>
-        </>
+        <nav className="navbar_container">
+            <ul className="navbar">
+                <li><Link href={"/"} className={pathname == "/" ? "active_section" : ""}>home</Link></li>
+                <li><Link href={"about"} className={pathname == "/about" ? "active_section" : ""}>sobre mim</Link></li>
+                <li><Link href={"skills"} className={pathname == "/skills" ? "active_section" : ""}>skills</Link></li>
+                <li><Link href={"projects"} className={pathname == "/projects" ? "active_section" : ""}>projetos</Link></li>
+                <li><Link href={"contact"} className={pathname == "/contact" ? "active_section" : ""}>entre em contato</Link></li>
+                <div className="social_media">
+                    <Link href={"https://github.com/GuilhermeSoaresGarcia"} target="_blank">
+                        <Image
+                            src={GitHubSVG}
+                            alt="GitHub"
+                        />
+                    </Link>
+                    <Link href={"https://www.linkedin.com/in/guilhermesoaresgarcia/"} target="_blank">
+                        <Image
+                            src={LinkedinSVG}
+                            alt="Linkedin"
+                        />
+                    </Link>
+                </div>
+            </ul>
+        </nav>
     )
 }
